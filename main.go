509628c349f1eed9e619f812bce0274251307313
Josh Ghiloni <jghiloni@pivotal.io>
2018-02-22 09:30:53 -0700

    Refactor, add min delay

--- main.go
+++ main.go
@@ -12,40 +12,58 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var rng *rand.Rand
+func buildRequestHandler(rng *rand.Rand, minDelay int64, maxDelay int64) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		num := rng.Float64()
 
-func doRequest(w http.ResponseWriter, r *http.Request) {
-	num := rng.Float64()
+		var threshold float64
+		var err error
 
-	var threshold float64
-	var err error
+		threshStr := os.Getenv("SUCCESS_RATE")
+		if threshold, err = strconv.ParseFloat(threshStr, 64); err != nil || (threshold < 0 || threshold > 1) {
+			threshold = 0.9
+		}
 
-	threshStr := os.Getenv("ERROR_THRESHOLD")
-	if threshold, err = strconv.ParseFloat(threshStr, 64); err != nil || (threshold < 0 || threshold > 1) {
-		threshold = 0.9
-	}
+		fmt.Printf("The threshold is %v and the random number is %v\n", threshold, num)
 
-	fmt.Printf("The threshold is %v and the random number is %v\n", threshold, num)
+		delay := int64(0)
+		if maxDelay > 0 {
+			delay = rng.Int63n(maxDelay) + minDelay
+		}
 
-	if num <= threshold {
-		w.WriteHeader(200)
+		fmt.Printf("Adding a randomly selected %v ms delay\n", delay)
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+
+		if num <= threshold {
+			w.WriteHeader(200)
+			w.Header().Set("Content-Type", "text/plain;charset=UTF-8")
+			w.Write([]byte("The request was a success 😁"))
+			return
+		}
+
+		w.WriteHeader(500)
 		w.Header().Set("Content-Type", "text/plain;charset=UTF-8")
-		w.Write([]byte("The request was a success 😁"))
+		w.Write([]byte("There was an unexpected error 😭"))
 		return
 	}
-
-	w.WriteHeader(500)
-	w.Header().Set("Content-Type", "text/plain;charset=UTF-8")
-	w.Write([]byte("There was an unexpected error 😭"))
-	return
 }
 
 func main() {
 	source := rand.NewSource(time.Now().UnixNano())
-	rng = rand.New(source)
+	rng := rand.New(source)
+
+	maxDelay, err := strconv.ParseInt(os.Getenv("MAX_DELAY_MS"), 10, 64)
+	if err != nil || maxDelay < 0 {
+		maxDelay = 0
+	}
+
+	minDelay, err := strconv.ParseInt(os.Getenv("MIN_DELAY_MS"), 10, 64)
+	if err != nil || minDelay < 0 {
+		minDelay = 0
+	}
 
 	r := mux.NewRouter()
-	r.PathPrefix("/").HandlerFunc(doRequest)
+	r.PathPrefix("/").HandlerFunc(buildRequestHandler(rng, minDelay, maxDelay))
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), r)
 }
